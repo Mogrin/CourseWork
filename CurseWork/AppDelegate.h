4210e39508b1a6fdc3a1043b9e41baf80172ffdf
KonstEmelyantsev <konstemel@macbook-pro-konstemelyantsev.local>
2016-04-17 18:44:36 +0300

    Initial Commit

---
+++ CurseWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurseWork
+//
+//  Created by KonstEmelyantsev on 4/17/16.
+//  Copyright © 2016 KonstEmelyantsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurseWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurseWork
+//
+//  Created by KonstEmelyantsev on 4/17/16.
+//  Copyright © 2016 KonstEmelyantsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
